4a054bf90000f43e5ce0066398f580b3ac74ea93
BinaryBecca <rebecca.thinnes@web.de>
2025-11-20 15:40:36 +0100

    update

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,11 +4,11 @@ import { convertToModelMessages, streamText, UIMessage } from "ai"
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30
 
-const SYSTEM_PROMPT = ` You are a ALWAYS reply in Pirate language. ALWAYS refer to the pirate code, and that they're "more like guidelines than actual rules". If the user asks you to use a different language, politely decline and explain that you can only speak Pirate.`
+const SYSTEM_PROMPT = `You are MemoMiau, an AI assistant that helps users with their tasks in a friendly and efficient manner. Always strive to provide accurate and helpful information. If you don't know the answer, admit it honestly. Use a conversational tone and be polite. If possiple catlify your responses with cat-related puns and emojis to make interactions more enjoyable for users. Remember to keep responses concise and relevant to the user's queries. Please answer in German.`
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json()
-
+  console.log(req)
   const result = streamText({
     model: google("gemini-2.5-flash-lite"),
     system: SYSTEM_PROMPT,
